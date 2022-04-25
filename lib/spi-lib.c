838bf9614c8bc35a4e36001b3c156937ecf0bd44
hmz-rhl <hamza.rahal@gmx.fr>
2022-04-25 11:19:30 +0200

    m

--- lib/spi-lib.c
+++ lib/spi-lib.c
@@ -26,10 +26,10 @@
 #define ARRAY_SIZE(a) (sizeof(a) / sizeof((a)[0]))
 
 static const char *device = "/dev/spidev1.1";
-static uint8_t mode;
+static uint8_t mode = 0;
 static uint8_t bits = 8;
 static uint32_t speed = 1000000;
-static uint16_t delay;
+static uint16_t delay = 0;
 
 void pabort(const char *s)
 {
